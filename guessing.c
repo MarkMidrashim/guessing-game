2039df25a1828967db7844bbd626ce09d38fc88c
MarkMidrashim <46815327+MarkMidrashim@users.noreply.github.com>
2019-02-12 13:20:33 -0200

    Finalização da lógica de reiniciar jogo

--- guessing.c
+++ guessing.c
@@ -1,7 +1,9 @@
 // Libs
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 #include <stdlib.h>
+#include <ncurses.h>
 #include <stdbool.h>
 
 // Defines
@@ -9,17 +11,29 @@
 
 // Global's properties
 float average;
-int attempts = 0, win = 0, lose = 0;
+int attempts = 0, win = 0, lose = 0, guesses = 0;
 char newChance[CHANCE_LENGTH];
+
+void header() {
+    printf("*****************\n");
+    printf("* Guessing Game *\n");
+    printf("*****************\n");
+}
 
 bool game(int secretNumber) {
     // Local's properties
     int attemptsForGame = 3, bid;
     
     do {
-        printf("| Number of guesses: %d |\n", attemptsForGame);
+        // Title
+        if (attemptsForGame == 3) {
+            header();
+        }
+        
+        printf("\n| Attempts: %d |\n", attemptsForGame);
         printf("What is your guess? ");
         scanf("%d", &bid);
+        guesses++;
         
         if (bid == secretNumber) {
             win++;
@@ -33,48 +47,74 @@ bool game(int secretNumber) {
                 return false;
             }
             else {
-                printf("Fail!\n\n");
+                // Clear console
+                system("clear");
+                
+                if (attemptsForGame < 3) {
+                    header();
+                }
+                
+                printf("You missed! ");
+                
+                // Tips
+                if (bid > secretNumber) {
+                    printf("%c[3mYour guess is greater than secret value.\n%c[0m", 27, 27);
+                }
+                
+                if (bid < secretNumber) {
+                    printf("%c[3mYour guess is smaller than secret value.\n%c[0m", 27, 27);
+                }
             }
         }
     } while (attemptsForGame > 0);
 }
 
+void analytics() {
+    // Clear console
+    system("clear");
+    
+    // Calculation
+    int total = win + lose;
+    double percentWin = ((double)win / (double)total) * 100;
+    double percentLose = ((double)lose / (double)total) * 100;
+    
+    printf("****************\n");
+    printf("* Game Results *\n");
+    printf("****************\n");
+    printf("Total Games: %d\n", total);
+    printf("Guesses: %d\n", guesses);
+    printf("Lose: %d\n", lose);
+    printf("Win: %d\n", win);
+    printf("--- Percents ---\n");
+    printf("Win: %.2f%%\n", percentWin);
+    printf("Lose: %.2f%%\n", percentLose);
+}
+
 int main() {
-    // Title
-    printf("*****************\n");
-    printf("* Guessing Game *\n");
-    printf("*****************\n\n");
-    
-    // Generate random number
-    srand(time(NULL));
-    int secretNumber = rand() % 10;
-    
     // Start game's logical
     do {
+        // Generate random number
+        srand(time(NULL));
+        int secretNumber = rand() % 10;
+        
         bool result = game(secretNumber);
         
         if (attempts == 0) {
-            printf("%s.", (result ? "Congratulations! You Win" : "Sorry! You Lose"));
+            printf("\n%s.", (result ? "Congratulations! You Win" : "Sorry! You Lose"));
             printf("\n\n");
         }
         
         printf("Do you want a new chance? (y/n) ");
-        scanf("%s", &newChance);
-            
-        if (newChance == "y") {
-            continue;
-        }
-        else {
+        scanf("%s", &newChance[0]);
+        
+        if (strcmp("n", newChance) == 0) {
             break;
         }
-            
-        // Clear variable value
-        // newChance.ignore();
-    } while (newChance == "n");
+        else {
+            system("clear");
+        }
+    } while (true);
     
     // Analytics
-    printf("\n\n");
-    printf("**********************\n");
-    printf("* Result's Analytics *\n");
-    printf("**********************\n");
+    analytics();
 }
